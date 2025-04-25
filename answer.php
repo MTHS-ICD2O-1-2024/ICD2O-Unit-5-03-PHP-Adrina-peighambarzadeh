02e42d2ee39fb1ed68e3b3e581afdcc4ffb2815e
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-25 01:34:42 +0000

    added stuff

--- answer.php
+++ answer.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
   <meta charset="utf-8" />
   <meta name="description" content="The Random Number guess, with PHP" />
-  <meta name="keywords" content="mths, icd2o" />
-  <meta name="author" content="Adrina peighambarzadeh" />
+  <meta name="keywords" content="math, salary, PHP, income" />
+  <meta name="author" content="Adrina.Peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.red-orange.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
@@ -27,27 +27,30 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/dice.gif" alt="Dice gif image" width="500" />
+        <img src="./images/Screenshot 2025-04-02 12.44.35.png" alt="sphere" width="300" />
       </div>
-      <br />
-      <div class="page-content-guide">Please enter indgree between 1 to 6:</div>
       <div class="page-content-php">
-        <form action="./answer.php" method="GET">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="user-number" />
-            <label class="mdl-textfield__label" for="user-number">Number between 1 to 6</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
-          <!-- Accent-colored raised button with ripple -->
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            type="submit">
-            Check
-          </button>
-        </form>
-      </div>
-    </main>
-  </div>
+        <div id="employment-info">
+          <?php
+
+          $result = "";
+          $randomNumber = rand(1, 6);
+
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $userNumber = isset($_POST["user-number"]) ? (int)$_POST["user-number"] : 0;
+
+            if ($userNumber == $randomNumber) {
+              $result = "You have guessed the correct number!";
+            } else {
+              $result = "You have guessed the wrong number!";
+            }
+          }
+          ?>
+          <input type="number" id="user-number" name="user-number" min="1" max="6" required>
+          <input type="submit" value="Check">
+          </form>
+
+          <p id="result"><?php echo $result; ?></p>
 </body>
 
 </html>
